da2174600bee6546480dd2dac82af1b26bf8b705
zianur-6am <zianur.6amtech@gmail.com>
2024-10-15 18:26:26 +0600

    designed listview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,70 +36,71 @@ class _HomePageState extends State<HomePage> {
 
               Tasks task = taskController.tasks[index];
 
-              return ListTile(
-                onTap: (){
-                  Get.bottomSheet(
-                      Container(
-                        child: Wrap(
-                          children: [
-                            ListTile(
-                              leading: const Icon(Icons.edit),
-                              title: const Text("Edit Task"),
-                              onTap: () {
-
-                                Get.back();
-
-                                //passing object to another screen
-                                Get.toNamed('/update_task',
-                                  arguments: task
-                                );
-
-
-
-                              },
-                            ),
-                            ListTile(
-                              leading: const Icon(Icons.delete),
-                              title: const Text("Delete Task"),
-                              onTap: (){
-                                taskController.deleteTaskController(task.id);
-                                // setState(() {
-                                //
-                                // });
-
-                                Get.back();
-                              },
-                            )
-                          ],
+              return Card(
+                elevation: 50,
+                shadowColor: Colors.blue,
+                margin: EdgeInsets.all(8),
+                child: ListTile(
+                  onTap: (){
+                    Get.bottomSheet(
+                        Container(
+                          child: Wrap(
+                            children: [
+                              ListTile(
+                                leading: const Icon(Icons.edit),
+                                title: const Text("Edit Task"),
+                                onTap: () {
+                
+                                  Get.back();
+                
+                                  //passing object to another screen
+                                  Get.toNamed('/update_task',
+                                    arguments: task
+                                  );
+                
+                
+                
+                                },
+                              ),
+                              ListTile(
+                                leading: const Icon(Icons.delete),
+                                title: const Text("Delete Task"),
+                                onTap: (){
+                                  taskController.deleteTaskController(task.id);
+                                  // setState(() {
+                                  //
+                                  // });
+                
+                                  Get.back();
+                                },
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-
-                      //properties of bottomsheet
-
-                      // barrierColor: Colors.blue,
-                      backgroundColor: Colors.orange,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                        //borderRadius: BorderRadius.circular(10),
-                      )
-                  );
-
-                },
-                title: Text(task!.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                subtitle: Text(task.description, style: TextStyle(fontSize: 14),),
-                trailing: Checkbox(
-                  //assigning the value to the ui
-                    value: task.status == 1,
-                    onChanged: (val){
-                      taskController.updateTaskController(task.id, task.title, task.description,  (val == true ? 1 : 0));
-                      // setState(() {
-                      // });
-                    }),
-                contentPadding: EdgeInsets.all(10),
-                shape: const Border(
-                  bottom: BorderSide(),
+                
+                        //properties of bottomsheet
+                
+                        // barrierColor: Colors.blue,
+                        backgroundColor: Colors.orange,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                          //borderRadius: BorderRadius.circular(10),
+                        )
+                    );
+                
+                  },
+                  title: Text(task!.title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  subtitle: Text(task.description, style: TextStyle(fontSize: 14),),
+                  trailing: Checkbox(
+                    //assigning the value to the ui
+                      value: task.status == 1,
+                      onChanged: (val){
+                        taskController.updateTaskController(task.id, task.title, task.description,  (val == true ? 1 : 0));
+                        // setState(() {
+                        // });
+                      }),
+                  contentPadding: EdgeInsets.all(10),
                 ),
-
               );
 
         });
